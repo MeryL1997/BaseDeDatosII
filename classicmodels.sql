29a5d47aefca2ab4f9d90f4beb2c7b50037a6d34
MeryL1997 <mery.lema@pucese.edu.ec>
2020-06-10 17:35:37 -0500

    creacionde vista2 vista de classicmodels

--- classicmodels.sql
+++ classicmodels.sql
@@ -7941,6 +7941,25 @@ group by p.productCode
 order by total_ventas desc;
 
 Use classicmodels;
+CREATE VIEW Cliente_mayor_cantidad_compras AS
+    SELECT 
+        COUNT(o.customerNumber) AS cantVentas,
+        c.customerName AS customerName,
+        SUM(od.quantityOrdered * od.priceEach) AS VentaTotal,
+        COUNT(DISTINCT od.productCode) AS ProductosDiferentes
+    FROM
+        ((classicmodels.customers c
+        JOIN classicmodels.orders o)
+        JOIN classicmodels.orderdetails od)
+    WHERE
+        c.customerNumber = o.customerNumber
+            AND o.orderNumber = od.orderNumber
+            AND o.orderDate BETWEEN CAST('2003-05-31' AS DATE) AND CAST('2004-05-31' AS DATE)
+    GROUP BY c.customerNumber
+    ORDER BY COUNT(o.customerNumber) DESC
+    LIMIT 1
+
+Use classicmodels;
 create procedure OrdenarProducto(
 orderNumberP int (11),
 orderDateP date ,
@@ -7948,7 +7967,7 @@ requieredDateP date ,
 shippedDateP date ,
 statusP varchar(15) ,
 commentsP text,
-customerNumberP int(11) ,
+customerNumberP int(11) 
 )
 As
 begin
